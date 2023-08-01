d4a3bcb11bec1dede8a52af86be6601f88ef3da3
Vinicius Sacramento <viniciusjose57@gmail.com>
2023-08-01 01:28:24 +0100

    adding smart contract to deploy migration

--- migrations/2_deploy_tokens.js
+++ migrations/2_deploy_tokens.js
@@ -1,6 +1,10 @@
 const AnyToken = artifacts.require("AnyToken")
+const FarmToken = artifacts.require("FarmToken")
 
 module.exports = async function (deployer, network, accounts) {
   await deployer.deploy(AnyToken)
   const anyToken = await AnyToken.deployed()
+
+  await deployer.deploy(FarmToken, anyToken.address)
+  const farmToken = await FarmToken.deployed()
 }
